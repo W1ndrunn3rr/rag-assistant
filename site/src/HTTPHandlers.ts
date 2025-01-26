0b490a9eb9cd4b9b128c412e643905e68bb84214
Jakub <w1ndrunn3rr@gmail.com>
2025-01-26 19:07:30 +0100

    gcp deploy

--- site/src/HTTPHandlers.ts
+++ site/src/HTTPHandlers.ts
@@ -1,5 +1,5 @@
 export async function handleSubmit(message: string) {
-    const url = `http://localhost:8000/invoke?message=${encodeURIComponent(message)}`;
+    const url = `https://rag-assistant-api-754277840579.europe-central2.run.app/invoke?message=${encodeURIComponent(message)}`;
     try {
         const response = await fetch(url, { method: 'POST' });
         if (response.ok) {
@@ -17,7 +17,7 @@ export async function handleSubmit(message: string) {
 export async function handleFileUpload(file: File) {
     const formData = new FormData();
     formData.append('pdf', file);
-    const url = `http://localhost:8000/upload`;
+    const url = `https://rag-assistant-api-754277840579.europe-central2.run.app/upload`;
     try {
         const response = await fetch(url, { method: 'POST', body: formData });
         if (response.ok) {
