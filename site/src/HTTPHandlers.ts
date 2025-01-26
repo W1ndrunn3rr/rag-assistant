350f760c246192113f3203760448862690532999
Jakub <w1ndrunn3rr@gmail.com>
2025-01-26 19:29:37 +0100

    please run

--- site/src/HTTPHandlers.ts
+++ site/src/HTTPHandlers.ts
@@ -1,15 +1,20 @@
 export async function handleSubmit(message: string) {
-    const url = `https://rag-assistant-api-754277840579.europe-central2.run.app/invoke?message=${encodeURIComponent(message)}`;
+    const url = `https://rag-assistant-api-754277840579.europe-central2.run.app/invoke`;
     try {
-        const response = await fetch(url, { method: 'POST' });
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ message: message })
+        });
         if (response.ok) {
             const data = await response.json();
             return data;
         } else {
-            throw new Error('Something went wrong');
+            throw new Error('Request failed');
         }
-    }
-    catch (error) {
+    } catch (error) {
         console.error(error);
     }
 }
@@ -19,15 +24,17 @@ export async function handleFileUpload(file: File) {
     formData.append('pdf', file);
     const url = `https://rag-assistant-api-754277840579.europe-central2.run.app/upload`;
     try {
-        const response = await fetch(url, { method: 'POST', body: formData });
+        const response = await fetch(url, {
+            method: 'POST',
+            body: formData
+        });
         if (response.ok) {
             const data = await response.json();
             return data;
         } else {
-            throw new Error('Something went wrong');
+            throw new Error('Upload failed');
         }
-    }
-    catch (error) {
+    } catch (error) {
         console.error(error);
     }
 }
